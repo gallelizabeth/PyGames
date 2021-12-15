f10b5de61d52f2d729b661719674946b7dfaa006
gallelizabeth <liza.a.gall@gmail.com>
2021-12-15 19:00:16 +0300

    бомбы не двигаются, но взрываются

--- sprits.py
+++ sprits.py
@@ -29,29 +29,34 @@ def load_image(name, color_key=None):
 
 class Bomb(pygame.sprite.Sprite):
     image = load_image("bomb.png")
+    image_boom = load_image("boom.png")
 
     def __init__(self, *group):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
-        # Это очень важно!!!
         super().__init__(*group)
         self.image = Bomb.image
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(width)
         self.rect.y = random.randrange(height)
 
+    def update(self, *args):
+        # self.rect = self.rect.move(random.randrange(3) - 1, random.randrange(3) - 1)
+        if args and args[0].type == pygame.MOUSEBUTTONUP and self.rect.collidepoint(args[0].pos):
+            self.image = Bomb.image_boom
+
 
 all_sprites = pygame.sprite.Group()
+
 for _ in range(50):
     Bomb(all_sprites)
 
 running = True
 while running:
     for event in pygame.event.get():
+        all_sprites.update(event)
         if event.type == pygame.QUIT:
             running = False
     screen.fill((0, 0, 0))
     all_sprites.draw(screen)
-    all_sprites.update()
     pygame.display.flip()
 
 pygame.quit()
